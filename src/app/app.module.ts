a8c1d0e8a6dbe962f85530d50183d3797714b8b0
Mendes Sousa <leoprofessionallogytech@outlook.com>
2022-08-11 15:08:21 -0300

    primeiro dia de aula do projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,26 @@
+import { TodoListComponent } from './modules/home/components/todo-list/todo-list.component';
+import { TodoInputAddItensComponent } from './modules/home/components/todo-input-add-itens/todo-input-add-itens.component';
+import { HeaderComponent } from './modules/home/components/header/header.component';
+import { TodoButtonDeleteAllComponent } from './modules/home/components/todoButtonDeleteAll/todo-button-delete-all.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+//Modules
+import { HomeComponent } from './modules/home/pages/home/home.component';
+
+
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    HeaderComponent,
+    TodoInputAddItensComponent,
+    TodoListComponent,
+    TodoButtonDeleteAllComponent,
+    HomeComponent
+ ],
   imports: [
     BrowserModule,
     AppRoutingModule
